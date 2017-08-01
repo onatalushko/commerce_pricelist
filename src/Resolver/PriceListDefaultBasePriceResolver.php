35814c20c44e2f9422f8970bc739cc413652470d
Heath Noel <heath@alpinestudios.co>
2017-08-01 15:18:16 -0600

    Fixed price resolver and changed commerce_pricing to commerce_price.

--- src/Resolver/PriceListDefaultBasePriceResolver.php
+++ src/Resolver/PriceListDefaultBasePriceResolver.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\commerce_pricelist\Resolver;
 
+use Drupal\commerce\Context;
 use Drupal\commerce\PurchasableEntityInterface;
-use Drupal\commerce_price\Plugin\Field\FieldType\Price;
+use Drupal\commerce_price\Price;
+use Drupal\commerce_price\Resolver\PriceResolverInterface;
 
 
 /**
@@ -11,27 +13,21 @@ use Drupal\commerce_price\Plugin\Field\FieldType\Price;
  *
  * @package Drupal\commerce_pricelist
  */
-class PriceListDefaultBasePriceResolver implements PriceListBasePriceResolverInterface {
+class PriceListDefaultBasePriceResolver implements PriceResolverInterface {
 
   /**
    * {@inheritdoc}
    */
-  public function resolve(PurchasableEntityInterface $entity, $quantity = 1) {
-    return $this->applies($entity) ? $entity->price->first() : NULL;
+  public function resolve(PurchasableEntityInterface $entity, $quantity = 1, Context $context) {
+    $price = new Price("45.34", $entity->getPrice()->getCurrencyCode());
+    return $this->applies($entity) ? $price : NULL;
   }
 
   /**
-   * Determines whether the resolver applies to the given purchasable entity.
-   *
-   * @param \Drupal\commerce\PurchasableEntityInterface $entity
-   *   The purchasable entity.
-   *
-   * @return bool
-   *   TRUE if the resolver applies to the given purchasable entity, FALSE
-   *   otherwise.
+   * {@inheritdoc}
    */
   public function applies(PurchasableEntityInterface $entity) {
-    return $entity->hasField('price') && !$entity->get('price')->isEmpty();
+    return $entity->hasField('price') && !$entity->getPrice()->isEmpty();
   }
 
 }
